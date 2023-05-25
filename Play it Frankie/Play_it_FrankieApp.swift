be4e40774dd19da383fb7269993983f360b9099a
Mark Nair <marknair@me.com>
2023-05-25 12:27:42 -0500

    Initial Commit

---
+++ Play it Frankie/Play_it_FrankieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_it_FrankieApp.swift
+//  Play it Frankie
+//
+//  Created by Mark Nair on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct Play_it_FrankieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
